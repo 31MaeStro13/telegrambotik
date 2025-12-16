bbb713f41c6ad37c243db0f9a7fc79ff10f51441
Oleg Irshko <irshko@sfedu.ru>
2025-12-16 19:58:43 +0300

    echo_bot

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-from aiogram import Bot, Dispatcher, types
+from aiogram import Bot, Dispatcher
+from aiogram.filters import Command
+from aiogram.types import Message
+
+TOKEN_API = "8023808359:AAG5zTPjmaBqaFoXqjKP3ywCZHyhfVVQaeE"
+
+bot = Bot()
+dp = Dispatcher
+
+@dp.message()
+def echo(message: Message.types):
+    message.answer(text=message.text())
+
+
+if __name__ == "__main__":
+    dp.run_polling()
