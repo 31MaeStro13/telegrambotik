6119a503f2797926c6895809d213bf0b95e256de
Oleg Irshko <irshko@sfedu.ru>
2025-12-16 20:02:24 +0300

    new config and git ingore

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 from aiogram import Bot, Dispatcher
 from aiogram.filters import Command
 from aiogram.types import Message
+from config.py import TOKEN_API # type: ignore
 
-TOKEN_API = "8023808359:AAG5zTPjmaBqaFoXqjKP3ywCZHyhfVVQaeE"
-
-bot = Bot()
-dp = Dispatcher
+bot = Bot(TOKEN_API)
+dp = Dispatcher()
 
 @dp.message()
 def echo(message: Message.types):
